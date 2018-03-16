17c30c660d8f641460ba528faf44004a3be79158
jrambold <jrambold@gmail.com>
2018-03-16 10:57:43 -0600

    can display specials

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -1,7 +1,8 @@
 class LaughTracksApp < Sinatra::Base
   get '/comedians' do
     comedians = Comedian.all
+    specials = Special.all
 
-    erb :'comedians/index', locals: { comedians: comedians }
+    erb :'comedians/index', locals: { comedians: comedians, specials: specials }
   end
 end
--- spec/features/comedians_spec.rb
+++ spec/features/comedians_spec.rb
@@ -1,15 +1,22 @@
-RSpec.describe Invoice do
+RSpec.describe Comedian do
   describe 'When visiting' do
 
     describe 'read' do
-      it 'can see names' do
+      it 'can see names and ages' do
         visit '/comedians'
 
         expect(current_path).to eq('/comedians')
         expect(page).to have_content('Iliza Shlesinger')
-        expect(page).to have_content('34')
+        expect(page).to have_content(34)
         expect(page).to have_content('Mike Birbiglia')
-        expect(page).to have_content('39')
+        expect(page).to have_content(39)
+      end
+      it 'can see specials' do
+        visit '/comedians'
+
+        expect(current_path).to eq('/comedians')
+        expect(page).to have_content('War Paint')
+        expect(page).to have_content('Freezing Hot')
       end
     end
   end
